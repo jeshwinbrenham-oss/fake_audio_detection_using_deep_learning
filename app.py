38b25614e5f3714a1d2beaa7450a0ff05c0b981b
Jeshwin Brenham <jeshwinbrenham@gmail.com>
2026-07-22 22:36:19 +0530

    Add Render deployment config

--- app.py
+++ app.py
@@ -134,4 +134,4 @@ if __name__ == "__main__":
     print("  http://127.0.0.1:5000")
     print("=" * 60 + "\n")
     init_db()
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
